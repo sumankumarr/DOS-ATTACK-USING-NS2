8ad0208f321ebb418b2b3e8443eebece62b04725
sumankumarr <140174594+sumankumarr@users.noreply.github.com>
2023-07-21 21:03:58 +0530

    Update prod_tcp.tcl

--- Attack and Entropy Calculation/prod_tcp.tcl
+++ Attack and Entropy Calculation/prod_tcp.tcl
@@ -12,7 +12,6 @@ $ns trace-all $tracefile1
 #open the nam trace fileclear
 set namfile [open out.nam w]
 $ns namtrace-all $namfile
-
 #define a 'finish' procedure
 proc finish {} {
 	global ns tracefile1 namfile
